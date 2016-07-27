996e6fbb52dd745421bc3755ebf18d300a553485
wangxmxxx <617445014@qq.com>
2016-07-27 14:38:20 +0800

    7.27

--- javascripts/main.js
+++ javascripts/main.js
@@ -84,3 +84,66 @@
 *
 *
 */
+
+	
+	// 视频弹幕特效  部分功能  完整版在360云盘
+	function mu()//控制弹幕的颜色、字体、位置的随机生成
+	{
+		function $(id)
+		{
+			return document.getElementById(id);
+		}
+		var num=0;
+		var pmh=ds.offsetHeight;
+
+		var dd=document.createElement('div');
+		dd.setAttribute('class','ss');
+		dd.innerHTML=$('dm').value;
+		$('dm').value='';
+		ds.appendChild(dd);
+		dd.style.color='rgb('+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+','+Math.floor(Math.random()*256)+')';
+		dd.style.fontSize=Math.floor(Math.random()*25+12)+'px';
+		dd.style.left=Math.floor(Math.random()*10+900)+'px';
+		dd.style.top=Math.floor(Math.random()*(pmh-30))+'px';
+		var l=pmh-dd.offsetWidth;
+		var tim=null;
+		tim=setInterval(function()
+		{
+	       l--;
+	       if(l<=(0-dd.offsetWidth))
+	       {
+	       	clearInterval(tim);
+	       	$('ds').removeChild(dd);
+	       }
+	       dd.style.left=l+'px';
+		},15) // 弹幕滚动速度
+	}
+	
+	// jsp页面代码
+	/**
+	<div id="ds"></div>
+	<div id="sent">
+		<input id="dm" type="text" onclick="ccc()" style="margin-left:160px;"/>
+		<input id="st" type="submit" onclick="mu()" value="发送 >>"/>		
+	</div>
+	*/
+	
+	// css 代码
+	/**
+	*{padding:0;margin:0;}
+	#zz{background-color: #5F2C6D;width:900px;height: 450px;margin: 0 auto;margin-top: 15px;color:#ffffff;}
+	#ds{position: relative;background-color: #5F2C6D;width:900px;height: 450px;margin: 0 auto;margin-top: 15px;color:#ffffff;overflow: hidden;}
+	.ss{position: absolute;top:10px;left: 20px;}
+	#sent{width:898px;height:70px;margin:0 auto;background: #ffffff;border-style:inset;border: 1px solid #d5d5d5;border-top:none;box-shadow:-2px 0  #dddddd,2px 0  #dddddd,0 2px  #dddddd;position: relative;}
+	#dm{width:498px;height:32px;margin:20px 0px 20px 10px;border:1px solid #d5d5d5;color:#6e6e6e; }
+	#st{line-height: 30px;color:#ffffff;border:none;background: #00A2D6;padding:0px 15px;text-align: center;cursor: pointer;}
+	#dw{position: absolute;left:208px;top:29px;font-size: 14px;color:#6e6e6e;}
+	#high{position: absolute;left:208px;top:30px;font-size: 12px;color:#B5B4B4;display: none;}
+	#yc{display:none;}
+	#xs{position: absolute;display: block;font-size: 18px;color:#6e6e6e;top:30px;left:300px;}
+	span{color:#ffa178;cursor: pointer;font-size: 20px;}
+	#back{width:100%;height: 100%;opacity: 0.6;background: #d5d5d5;position: absolute;top:0px;left:0px;display: none;}
+	input{border:none;background: #ffffff;color:#3c3c3c;border:1px solid #d5d5d5;padding-left: 10px;padding-right: 10px;line-height: 30px;}
+	#rr{line-height: 30px;color:#ffffff;border:none;background: #ddd;padding:0px 15px;text-align: center;cursor: pointer;margin-left: 90px;}
+	#rr{line-height: 30px;color:#ffffff;border:none;background: #ddd;padding:0px 15px;text-align: center;cursor: pointer; margin-left: 90px;}
+	*/
